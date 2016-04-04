bcf0687be8633a3b7b1ea3c64d5760573d1d2cb6
tigerbombz <madd1778@yahoo.com>
2016-04-04 10:51:13 -0700

    init repo routes-redux

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
+import PostsIndex from './components/post_index';
+
+
+
+export default (
+  <Route path='/' component={App}>
+    <IndexRoute component={PostsIndex} />
+  </Route>
+);
+
