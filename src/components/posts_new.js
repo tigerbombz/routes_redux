8cc6afa12793b41b90bc12dd41255bb086a5ae8e
tigerbombz <madd1778@yahoo.com>
2016-04-05 09:10:47 -0700

    added rerouting and cancel button

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,15 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  onSubmit(props){
+    this.props.createPost(props)
+      .then(() => {
+        //blog post has been created, navigate the user to the index
+        //we navigate by calling this.context.router.upsh with the
+        //new path to navigate to.
+        this.context.router.push('/');
+      });
+  }
+
+
   render() {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
 
 
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
 
         <h3>Create a New Post</h3>
 
